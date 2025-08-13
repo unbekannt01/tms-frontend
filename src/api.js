f4b3b87b822619b34cf591bf379b7c36a5d4bdb8
Prashant Kargathara <96536158+unbekannt01@users.noreply.github.com>
2025-08-13 17:43:15 +0530

    Update api.js
    Change Backend URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://nodejs-migration.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
