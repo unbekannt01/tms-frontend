70fab0968e83f9f11397e493af812b382c5c17af
Prashant <pkargathara7401@gmail.com>
2025-08-24 11:31:38 +0530

    update with backend link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://nodejs-migration.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
